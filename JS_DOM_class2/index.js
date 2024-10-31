475c3a16796a3e78f2c35b505ca076b9821b3da8
manojz18 <manojzagade16@gmail.com>
2024-10-31 21:50:56 +0530

    few more files and folders added

---
+++ JS_DOM_class2/index.js
@@ -0,0 +1,12 @@
+// document.addEventListener('click', function(){
+//     console.log("Hello Bhava");
+// });
+
+/// ============= OR ============
+
+document.addEventListener('click', addThis);
+
+function addThis(){
+    console.log("Hello Friend");
+
+}
